9a0adccb9c58694f01a4b067b3ad7a06fdc16996
capsSadSack <cherkashin@phystech.edu>
2021-06-16 09:22:26 +0300

    Empty Xamarin Project

---
+++ Woodnote/WoodnoteMobileUI/WoodnoteMobileUI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using WoodnoteMobileUI.ViewModels;
+
+namespace WoodnoteMobileUI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Woodnote/WoodnoteMobileUI/WoodnoteMobileUI/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using WoodnoteMobileUI.Models;
+using WoodnoteMobileUI.ViewModels;
+
+namespace WoodnoteMobileUI.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
