dfe13f21f2ab3f8e9f78fa1b8efce5f5b04de20e
capsSadSack <cherkashin@phystech.edu>
2021-06-19 13:30:55 +0300

    Colors in bird search page

---
+++ Woodnote/WoodnoteMobileUI/WoodnoteMobileUI/Views/BirdSearch/ColorSelectionView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WoodnoteMobileUI.Views.BirdSearch
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColorSelectionView : ContentView
+    {
+        public ColorSelectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
