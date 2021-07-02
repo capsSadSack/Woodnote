e440d0a59ef4f22210026e8abd9406d672b9d988
capsSadSack <cherkashin@phystech.edu>
2021-07-02 13:42:14 +0300

    Vector images in psd, new image resolution - 300 x 400

--- Woodnote/WoodnoteMobileUI/WoodnoteMobileUI/Views/BirdSearch/SilhouetteView.xaml.cs
+++ Woodnote/WoodnoteMobileUI/WoodnoteMobileUI/Views/BirdSearch/SilhouetteView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,12 +8,32 @@ namespace WoodnoteMobileUI.Views.BirdSearch
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SilhouetteView : ContentView
     {
+        private bool _isSelected = false;
+
+
         public SilhouetteView(string imageFileName, string imageLabel)
         {
             InitializeComponent();
 
             image.Source = ImageSource.FromResource(imageFileName, this.GetType().GetTypeInfo().Assembly);
             label.Text = imageLabel;
+        }
+
+
+        private void OnTapped(object sender, EventArgs e)
+        {
+            _isSelected = !_isSelected;
+
+            if (_isSelected)
+            {
+                silhouetteFrame.BorderColor = Color.DeepSkyBlue;
+                silhouetteFrame.BackgroundColor = Color.LightBlue;
+            }
+            else
+            {
+                silhouetteFrame.BorderColor = Color.Black;
+                silhouetteFrame.BackgroundColor = Color.Transparent;
+            }
         }
     }
 }
