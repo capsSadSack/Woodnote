9982defc388cbfcaf3b9927cb23c2bad8e3c2f02
capsSadSack <cherkashin@phystech.edu>
2021-09-06 23:34:32 +0300

    Refactoring & Sync

--- Woodnote/WoodnoteWPF/ViewModels/BirdSearchViewModel.cs
+++ Woodnote/WoodnoteWPF/ViewModels/BirdSearchViewModel.cs
@@ -197,8 +197,19 @@ namespace WoodnoteWPF.ViewModels
             item.IsSelected = !item.IsSelected;
         }
 
-
-        public void Polygon_MouseDown()
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                    (addCommand = new RelayCommand(obj =>
+                    {
+                        int i = 0;
+                    }));
+            }
+        }
+        public void Polygon_MouseDown(PolygonModel polygon)
         {
             
 
@@ -213,6 +224,37 @@ namespace WoodnoteWPF.ViewModels
         public void OnDeselectRegionClicked(RegionModel item)
         {
             item.IsSelected = !item.IsSelected;
+        }
+
+
+    }
+
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
         }
     }
 }
--- Woodnote/WoodnoteWPF/Views/BirdSearchView.xaml.cs
+++ Woodnote/WoodnoteWPF/Views/BirdSearchView.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace WoodnoteWPF.Views
 {
@@ -12,10 +7,6 @@ namespace WoodnoteWPF.Views
     /// </summary>
     public partial class BirdSearchView : UserControl
     {
-        private double _maxWidth = 600;
-        private double _maxHeight = 800;
-
-
         public BirdSearchView()
         {
             InitializeComponent();
